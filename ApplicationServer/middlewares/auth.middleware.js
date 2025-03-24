36abf2dbe4ea07c068fba36d9d01ab252dc4cb69
VaibhavDhanani <vaibhavdhanani2711@gmail.com>
2025-03-24 21:07:44 +0530

    profile page done and indivisual chats done

--- ApplicationServer/middlewares/auth.middleware.js
+++ ApplicationServer/middlewares/auth.middleware.js
@@ -1,17 +1,16 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const authenticateToken = (req, res, next) => {
-  let token =null;
-  const authHeader = req.headers['authorization']; // Expect "Bearer <token>"
-   token = authHeader && authHeader.split(' ')[1];
- // If no token in header, check query parameter (for SSE requests)
- if (!token && req.query.token) {
-  token = req.query.token;
-}
-  if (!token) return res.status(401).json({ message: 'Access Denied' });
-
+  let token = null;
+  const authHeader = req.headers["authorization"]; // Expect "Bearer <token>"
+  token = authHeader && authHeader.split(" ")[1];
+  // If no token in header, check query parameter (for SSE requests)
+  if (!token && req.query.token) {
+    token = req.query.token;
+  }
+  if (!token) return res.status(401).json({ message: "Access Denied" });
   jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
-    if (err) return res.status(403).json({ message: 'Invalid Token' });
+    if (err) return res.status(403).json({ message: "Invalid Token" });
     req.user = user; // Attach user info to request object
     next();
   });
